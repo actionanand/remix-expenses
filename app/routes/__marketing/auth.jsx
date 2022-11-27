5190695126b9f9d20bf7200715d6598ce3cdd3e1
Anand raja <meetanandns@gmail.com>
2022-11-27 20:05:00 +0000

    visual effect to login

--- app/routes/__marketing/auth.jsx
+++ app/routes/__marketing/auth.jsx
@@ -8,3 +8,19 @@ export default function AuthPage() {
 export function links() {
   return [{ rel: 'stylesheet', href: authStyles }];
 }
+
+export async function action({request}) {
+  const searchParams = new URL(request.url).searchParams;
+  const authMode = searchParams.get('mode') || login;
+
+  const formData = await request.formData();
+  const credentials = Object.fromEntries(formData);
+
+  // validate user inputs
+
+  if(authMode === 'login') {
+    // login logic
+  } else {
+    // sign up
+  }
+}
