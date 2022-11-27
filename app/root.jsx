cbf98dfacca019f3843ed012e5a2e97f6e552fb5
Anand raja <meetanandns@gmail.com>
2022-11-27 20:36:25 +0000

    catchBoundary added

--- app/root.jsx
+++ app/root.jsx
@@ -1,13 +1,7 @@
-import {
-  Links,
-  LiveReload,
-  Meta,
-  Outlet,
-  Scripts,
-  ScrollRestoration,
-} from '@remix-run/react';
+import { Links, Link, LiveReload, Meta, Outlet, Scripts, ScrollRestoration, useCatch } from '@remix-run/react';
 
 import sharedStyles from '~/styles/shared.css';
+import Error from './components/util/Error';
 
 export const meta = () => ({
   charset: "utf-8",
@@ -15,24 +9,48 @@ export const meta = () => ({
   viewport: "width=device-width,initial-scale=1",
 });
 
-export default function App() {
+export function Document({children, title='Remix-Expenses'}) {
   return (
     <html lang="en">
       <head>
         <Meta />
+        <title> {title} </title>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
         <link href="https://fonts.googleapis.com/css2?family=Rubik:wght@400;700&display=swap" rel="stylesheet" />
         <Links />
       </head>
       <body>
-        <Outlet />
+        {children}
         <ScrollRestoration />
         <Scripts />
         <LiveReload />
       </body>
     </html>
   );
+}
+
+export default function App() {
+  return <Document>
+    <Outlet />
+  </Document>;
+}
+
+export function CatchBoundary() {
+  const caughtResp = useCatch();
+
+  return <Document title={caughtResp.statusText}>
+    <main>
+      <Error title={caughtResp.statusText}>
+        <p> {caughtResp.data?.message || 'Something went wrong, Please try again after sometime!'} </p>
+        <p>Back to <Link to='/'> Safty </Link> . </p>
+      </Error>
+    </main>
+  </Document>;
+}
+
+export function ErrorBoundary() {
+
 }
 
 export function links() {
