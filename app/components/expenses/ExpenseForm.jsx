19cd9ae3fbeb8cb0fcf4f8769bc0ba287c7007f2
Anand raja <meetanandns@gmail.com>
2022-11-27 07:09:01 +0000

    visual fb for submitting form

--- app/components/expenses/ExpenseForm.jsx
+++ app/components/expenses/ExpenseForm.jsx
@@ -1,12 +1,24 @@
-import { Link, useActionData } from '@remix-run/react';
+import { Link, useActionData, Form, useSubmit, useTransition as useNavigation } from '@remix-run/react';
 
 function ExpenseForm() {
   const today = new Date().toISOString().slice(0, 10); // yields something like 2023-09-10
 
   const validationError = useActionData();
+  const submit = useSubmit();
+  const navigation = useNavigation();
+  const isSubmitting = navigation.state !== 'idle';
+
+  const submitHandler = e => {
+    e.preventDefault();
+    // perform client side validations
+    submit(e.target, {
+      // action: 'expenses/add',
+      method: 'post'
+    });
+  }
 
   return (
-    <form method="post" className="form" id="expense-form">
+    <Form method="post" className="form" id="expense-form" onSubmit={submitHandler}>
       <p>
         <label htmlFor="title">Expense Title</label>
         <input type="text" id="title" name="title" required maxLength={30} />
@@ -37,10 +49,10 @@ function ExpenseForm() {
       }
 
       <div className="form-actions">
-        <button>Save Expense</button>
+        <button disabled={isSubmitting}> { isSubmitting ?  'Saving...' : 'Save Expense' } </button>
         <Link to="..">Cancel</Link>
       </div>
-    </form>
+    </Form>
   );
 }
 
